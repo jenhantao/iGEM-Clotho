d6071c03963c7eaea129d2f62b3daf7a64325d41
jenhan <jenhan@sol.(none)>
2011-08-25 22:23:27 -0700

    added new File/New menu, removed SequenceView xml bug

--- ClothoBiofabEdition/ClothoFabApps/FabDash/src/org/clothocore/widget/fabdash/FileMenuPopulator.java
+++ ClothoBiofabEdition/ClothoFabApps/FabDash/src/org/clothocore/widget/fabdash/FileMenuPopulator.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import org.openide.util.actions.Presenter;
 
@@ -16,18 +17,77 @@ public class FileMenuPopulator extends AbstractAction implements Presenter.Menu 
 
     @Override
     public JMenuItem getMenuPresenter() {
-        JMenuItem m = new JMenuItem("About Clotho") {
+        JMenu m = new JMenu("New") {
         };
-        m.addActionListener(new ActionListener() {
+        JMenuItem partsItem = new JMenuItem("Part");
+        partsItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(ActionEvent ae) {
-                JFrame jf = new JFrame("moo");
-                jf.add(new JLabel("said the cow"));
-                jf.pack();
-                jf.setVisible(true);
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a part!");
             }
         });
+        m.add(partsItem);
+
+        JMenuItem oligoItem = new JMenuItem("Oligo");
+        oligoItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a oligo!");
+            }
+        });
+        m.add(oligoItem);
+
+        JMenuItem vectorItem = new JMenuItem("Vector");
+        vectorItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a vector!");
+            }
+        });
+        m.add(vectorItem);
+
+        JMenuItem plasmidItem = new JMenuItem("Plasmid");
+        plasmidItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a plasmid!");
+            }
+        });
+        m.add(plasmidItem);
+
+        JMenuItem featureItem = new JMenuItem("Feature");
+        featureItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a feature!");
+            }
+        });
+        m.add(featureItem);
+
+        JMenuItem sampleItem = new JMenuItem("Sample");
+        sampleItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a sample!");
+            }
+        });
+        m.add(sampleItem);
+
+        JMenuItem collectionItem = new JMenuItem("Collection");
+        collectionItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("I added a collection!");
+            }
+        });
+        m.add(collectionItem);
         return m;
     }
 
